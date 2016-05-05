8ba2f8ce0cc7a99d214a0c98d375aab08b587ad3
YuriSolovyov <biohazard707@gmail.com>
2016-05-05 15:37:15 +0300

    Add standalone example

---
+++ examples/index.rb
@@ -0,0 +1,17 @@
+require 'sinatra/base'
+require 'disel_router'
+
+module HelloRoutes
+  include Disel::Router
+  route :get, '/', :index
+end
+
+class HelloController < Sinatra::Base
+  include HelloRoutes
+
+  def index
+    'Hello!'
+  end
+
+  run!
+end
