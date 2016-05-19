f9b1cf13f0bb8246c727945d6c9f991ecd192dd4
YuriSolovyov <biohazard707@gmail.com>
2016-05-19 21:55:44 +0300

    Use per-module routes hash

--- lib/disel_router.rb
+++ lib/disel_router.rb
@@ -13,23 +13,18 @@ module Disel
   end
 
   module Router
-    ROUTES = Hash.new
 
     def self.included(mod)
+
       mod.module_eval do |m|
+        @routes = []
 
         def m.route(http_verb, *args, controller_method)
-          routes = ROUTES[self]
-          if routes.nil?
-            ROUTES[self] = []
-            routes = ROUTES[self]
-          end
-          routes << Route.new(http_verb, args, controller_method)
+          @routes << Route.new(http_verb, args, controller_method)
         end
 
         def m.included(controller)
-          routes = ROUTES[self]
-          routes.each do |route|
+          @routes.each do |route|
             controller.send(route.verb, *route.args) do |*args|
               self.send(route.method, *args)
             end
@@ -37,6 +32,8 @@ module Disel
         end
 
       end
+
     end
+
   end
 end
